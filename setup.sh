ef5df1543307718693c9035323c64d072d76dcf3
gabrielHawkins7 <gabe@Gabe-Dell.localdomain>
2025-08-06 11:55:40 -0400

    bye

--- setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-ln -s ~/.config/dotfiles/_nvim/nvim ~/.config/nvim
-ln -s ~/.config/dotfile/_nvim/textvim ~/.config/textvim
-
-echo "source ~/.config/dotfiles/.bashsetup" >> ~/.bashrc
